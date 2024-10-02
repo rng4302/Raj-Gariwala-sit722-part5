b3701629dc182cc5abb332bf444f836a87e1d567
Raj <rajgariwala40302@gmail.com>
2024-10-02 12:46:41 +1000

    testing 3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,6 @@ set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 
-echo "Deploying image: week9task2.azurecr.io/$NAME:$VERSION"
-
 # Update the image tag in the Kubernetes deployment file
 sed -i "s|image: $CONTAINER_REGISTRY/book:.*|image: $CONTAINER_REGISTRY/book:$VERSION|g" ./scripts/kubernetes/deployment.yaml
 sed -i "s|image: $CONTAINER_REGISTRY/inventory:.*|image: $CONTAINER_REGISTRY/inventory:$VERSION|g" ./scripts/kubernetes/deployment.yaml
