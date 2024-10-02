7f2fc4bb4f113f4e33387ce67d5b378a92e604d0
Raj <rajgariwala40302@gmail.com>
2024-10-02 13:14:29 +1000

    Testing 5

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,22 @@
+#!/bin/bash
 # Deploys a microservice to Kubernetes.
+
 set -u # or set -o nounset
-: "$CONTAINER_REGISTRY"
-: "$VERSION"
-echo "$CONTAINER_REGISTRY:$VERSION"
-# Update the image tag in the Kubernetes deployment file
-sed -i "s|image: $CONTAINER_REGISTRY/book:.*|image: $CONTAINER_REGISTRY/book:$VERSION|g" ./scripts/kubernetes/deployment.yaml
-sed -i "s|image: $CONTAINER_REGISTRY/inventory:.*|image: $CONTAINER_REGISTRY/inventory:$VERSION|g" ./scripts/kubernetes/deployment.yaml
+: "${CONTAINER_REGISTRY:?Need to set CONTAINER_REGISTRY}"
+: "${VERSION:?Need to set VERSION}"
+
+# Echo the values to ensure they're being passed correctly
+echo "Deploying images from: $CONTAINER_REGISTRY with version: $VERSION"
+
+# Update the image tag in the Kubernetes deployment file for book service
+sed -i "s|image: .*/book:.*|image: $CONTAINER_REGISTRY/book:$VERSION|g" ./scripts/kubernetes/deployment.yaml
+
+# Update the image tag in the Kubernetes deployment file for inventory service
+sed -i "s|image: .*/inventory:.*|image: $CONTAINER_REGISTRY/inventory:$VERSION|g" ./scripts/kubernetes/deployment.yaml
+
 # Apply the Kubernetes configuration
 kubectl apply -f ./scripts/kubernetes/deployment.yaml
+
+# Check the status of the deployment
+kubectl rollout status deployment/book-catalog-deployment
+kubectl rollout status deployment/inventory-management-deployment
